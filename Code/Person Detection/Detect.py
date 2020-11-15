e5b18f14f7d9db404293bca07f5e99aa071b5aae
nedwards0 <nedwards0001@gmail.com>
2020-11-15 14:13:16 -0800

    Somewhat working model

--- Code/Person Detection/Detect.py
+++ Code/Person Detection/Detect.py
@@ -1,4 +1,11 @@
 import cv2
 import numpy as np
 
-ney=cv2.dnn.readNet("yolov3.weights","yolov3.cfg")
+img =cv2.imread(r"0001.jpg",1)
+print (img.shape)
+cv2.imshow("Legend",img)
+cv2.waitKey(2000)
+cv2.destroyAllWindows
+img=cv2.resize(img,int(img))
+classNames=[]
+classFile=r"Code\Person Detection\coco.names"
