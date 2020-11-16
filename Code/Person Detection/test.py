67bbf5c7e44b32646a99b204a07e902e28e476da
mvezien <mattbobv@gmail.com>
2020-11-15 16:29:55 -0800

    11/15/2020

--- Code/Person Detection/test.py
+++ Code/Person Detection/test.py
@@ -1,7 +1,11 @@
 import cv2
 
-img=cv2.imread(r"C:\Users\nedwa\Documents\GitHub\Spotter\0001.jpg")
+img=cv2.imread(r"Images\Corrcet_0041.jpg")
+
+print(img.shape)
+
 cv2.imshow("Output",img)
+
 cv2.waitKey(2000)
 classNames=[]
 classFile=r"Code\Person Detection\coco.names"
@@ -17,6 +21,31 @@ net.setInputScale(1.0/ 127.5)
 net.setInputMean((127.5, 127.5, 127.5))
 net.setInputSwapRB(True)
 
-classIds,confs,bbox=net.detect(img,confThreshold=.3)
+classIds,confs,bbox=net.detect(img,confThreshold=.70)
 print(classIds)
 print(bbox)
+counter=0
+
+for x in classIds:
+    
+    for y in x:
+        print(y)
+        if y==1:
+            print('found person')
+            print(x)
+            print(counter)
+            temparray=bbox[counter]
+            
+            
+            print(temparray)
+            x1=temparray[0]
+            y1=temparray[1]
+            x2=temparray[2]
+            y2=temparray[3]
+
+            crop_img = img[y1:y2+200, x1:x2+200]
+            cv2.imshow("cropped", crop_img)
+            cv2.waitKey(2000)
+
+    
+    counter=counter+1
